0ce5e620b1424a5a069d6401e41cce50524a3e48
simidzu2ay <simidzu2ay@protonmail.com>
2024-02-29 18:46:33 +0100

    hypr: better autostart

--- .config/hypr/cleanup_after_startup.sh
+++
@@ -1,8 +0,0 @@
-sleep 5
-hyprctl keyword windowrule "workspace unset,firefox"
-hyprctl keyword windowrule "workspace unset,org.telegram.desktop"
-hyprctl keyword windowrule "workspace unset,class:Spotify"
-hyprctl keyword windowrule "workspace unset,class:discord"
-hyprctl keyword windowrule "workspace unset,class:foot"
-
-
