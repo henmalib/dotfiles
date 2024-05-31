1447db90b678a4a20a54fedcc0930250e5fc26c0
simidzu2ay <simidzu2ay@protonmail.com>
2024-05-31 19:29:38 +0200

    fish: ssh agent, corepack pnpm

--- .config/fish/conf.d/aliases.fish
+++ .config/fish/conf.d/aliases.fish
@@ -1,1 +1,2 @@
 alias ls="eza"
+alias pnpm="corepack pnpm"
--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,9 @@
 if status is-interactive
     export GPG_TTY=$(tty)
 end
+
+if not pgrep --full ssh-agent | string collect > /dev/null
+    eval (ssh-agent -c)
+    set -Ux SSH_AGENT_PID $SSH_AGENT_PID
+    set -Ux SSH_AUTH_SOCK $SSH_AUTH_SOCK
+end
