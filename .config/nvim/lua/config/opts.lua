2f66ad59e44fcfe379818fdd434a96b11f237dfd
simidzu2ay <simidzu2ay@protonmail.com>
2025-03-09 13:42:52 +0100

    cleanup

--- .config/nvim/lua/config/opts.lua
+++ .config/nvim/lua/config/opts.lua
@@ -18,7 +18,8 @@ opt.undofile = true
 opt.tabstop = 4
 opt.shiftwidth = 4
 opt.softtabstop = 4
--- opt.expandtab = true
+opt.expandtab = true
+opt.smartindent = true
 
 opt.wrap = false
 opt.scrolloff = 8
